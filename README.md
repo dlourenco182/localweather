9cb9a6eec752208cdc6cc79dfe29e61a825c9a91
David Lourenço <david.lourenco11@gmail.com>
2018-06-16 00:11:00 +0100

    Major Update
    API working with no contact from the user. just a simple call

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 localweather
+
+Picture credit: Photo by Orlova Maria <link>https://unsplash.com/photos/bcyC0eCFH8U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText</link> on Unsplash<link>https://unsplash.com/search/photos/sky?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText</link>
--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-function tellhim() {
-    document.getElementById('imcvalue').innerHTML = "";
-    document.getElementById('imctext').innerHTML = "";
-    var weigth = parseInt(document.getElementById('input_weight').value);
-    var height = parseInt(document.getElementById('input_height').value) * .01;
-    var result = weigth / (height * height);
-    result = parseFloat(result.toFixed(2));
+$(document).ready(function() {
 
-    document.getElementById('imcvalue').innerHTML = "<b>" + result + "</b>";
+    var long;
+    var lat;
+    var api;
 
-    if (result < 18.5) {
-        document.getElementById('imctext').innerHTML = "<b>Baixo Peso</b>";
-    } else if (result >= 18.5 && result <= 24.9) {
-        document.getElementById('imctext').innerHTML = "<b>Peso Ideal</b>";
-    } else if (result >= 25 && result <= 29.9) {
-        document.getElementById('imctext').innerHTML = "<b>Sobrepeso</b>";
-    } else if (result >= 30 && result <= 34.9) {
-        document.getElementById('imctext').innerHTML = "<b>Obesidade I</b>";
-    } else if (result >= 35 && result <= 39.9) {
-        document.getElementById('imctext').innerHTML = "<b>Obesidade II</b>";
-    } else if (result >= 40) {
-        document.getElementById('imctext').innerHTML = "<b>Baixo Peso</b>";
-    } else {
-        console.log("ERROR IMC NOT CALCULATED!");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            long = parseFloat(position.coords.longitude.toFixed(2));
+            lat = parseFloat(position.coords.latitude.toFixed(2));
+            api = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=80a02ad027f661042d5d7effacd085f2";
+            $.getJSON(api, function(data) {
+                var weatherType = data.weather[0].description;
+                var kelvin = data.main.temp;
+                var windSpeed = data.wind.speed;
+                var city = data.name;
+                var icon = '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">';
+                $("#inputweather").append("<br>City: " + city);
+                $("#inputweather").append("<br>Time: " + icon);
+                $("#inputweather").append("<br>Description: " + weatherType);
+                $("#inputweather").append("<br>Temp: " + kelvin + "(k)");
+            });
+        });
     }
-}
+});
